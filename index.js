81e1f40a349219067733a8c41f417feeb51b0d4a
IvSergey <imas.spb@mail.ru>
2019-03-18 13:20:46 +0300

    clone commit

--- index.js
+++ index.js
@@ -3,13 +3,39 @@ const State = {
     number : [],
 };
 const wrapper = document.getElementById('wrapper');
+// const buttonDelete = document.getElementById('delete');
+// buttonDelete.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     console.log('!!!!');
+//     // console.log(e.target.parentNode);
+//     // e.target.parentNode.remove(); 
 
-const addCondition = document.getElementById('add-condition');
+// })
+const templateWrapper = `<select name="fields" class="fields" >
+            <option value="Text field">Text field</option>
+            <option value="Number field">Number field</option>
+        </select>
+        <select class="text">
+            <option selected value="Containing">Containing</option>
+            <option value="Containing">Exactly matching</option>
+            <option value="Containing">Begins with</option>
+            <option value="Containing">Ends with</option>
+        </select>
+        <select class="number">
+            <option selected value="Equal">Equal</option>
+            <option value="Greater than">Greater than</option>
+            <option value="Less than">Less than</option>
+        </select>
+        <input type="text">
+        `
+
+const addCondition = document.querySelector('#add-condition');
 addCondition.addEventListener('click', (e) => {
     e.preventDefault();
-    var div = document.querySelector(".div-wrapper");
-    var newDiv = div.cloneNode(true);
-    div.parentNode.insertBefore(newDiv, null);
+    var newDiv = document.createElement('div');
+ 
+    newDiv.classList.add('div-wrapper');
+    newDiv.innerHTML = templateWrapper;
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent ='X';
@@ -19,8 +45,12 @@ addCondition.addEventListener('click', (e) => {
         wrapper.removeChild(newDiv);
     })
 
+     
      newDiv.appendChild(deleteButton);
-  
+     
+    // console.log(document.getElementById('wrapper'),'!!!');
+    wrapper.appendChild(newDiv);
+    console.log(wrapper.firstElementChild)
 })
 
 
@@ -30,15 +60,16 @@ const form = document.querySelector("#form-main");
 let fields = document.querySelector(".fields")
 
 
-fields[0].addEventListener("click", (e)=>{
+fields.addEventListener("click", (e)=>{
     if (form.elements.fields.value == "Number field"){
-        number[0].style.display="inline-block";
-        text[0].style.display="none";
+        number.style.display="inline-block";
+        text.style.display="none";
         e.preventDefault();
     }
     else if (form.elements.fields.value == "Text field"){
-        text[0].style.display="inline-block";
-        number[0].style.display="none";
+        text.style.display="inline-block";
+        number.style.display="none";
     }
-
+        
+    
 })
